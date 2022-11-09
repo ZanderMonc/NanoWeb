c78181323da084bac2656b04aa97219001206414
2555900M <2555900M@student.gla.ac.uk>
2022-11-09 11:57:58 +0000

    fixed syntax

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def data_upload(filename: str):
     import pandas as pd
     try:
         with open(filename) as data:
-            data = pd.read_csv("./data/titanic.csv")
-            st.write(data)
+            data_in = pd.read_csv(data)
+            st.write(data_in)
     except FileNotFoundError:
         st.error('File not found.')
