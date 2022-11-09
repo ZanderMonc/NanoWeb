4d8ca92aea58f9e0e81e0551ce3062b1769b3c5e
2555900M <2555900M@student.gla.ac.uk>
2022-11-09 12:21:28 +0000

    added basic csv export button for streamlit, and commented out test to implement

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import os
 
 
 def main() -> None:
@@ -10,7 +12,6 @@ if __name__ == "__main__":
 
 
 def data_upload(filename: str):
-    import pandas as pd
     try:
         with open(filename) as data:
             data_in = pd.read_csv(data)
@@ -18,3 +19,18 @@ def data_upload(filename: str):
             st.write('Data uploaded successfully')
     except FileNotFoundError:
         st.error('File not found.')
+
+
+def data_export(data, filename):
+    try:
+        with open(filename) as f:
+            st.download_button('Download CSV', f, 'data.csv')
+    except FileNotFoundError:
+        st.error('File not found.')
+
+
+# def export_test():
+#     df = pd.DataFrame({'a': [1, 2, 3], 'b': [4, 5, 6]})
+#     data_export(df, 'data.csv')
+
+
