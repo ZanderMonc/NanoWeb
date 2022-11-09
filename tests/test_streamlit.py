3915fe07ec4ddd16cc2f1d1ab34253eeaec64f82
2555900M <2555900M@student.gla.ac.uk>
2022-11-09 12:03:31 +0000

    Added test for file Input to be implemented later

--- tests/test_streamlit.py
+++ tests/test_streamlit.py
@@ -5,6 +5,9 @@ class TestStreamlit(unittest.TestCase):
     def test_streamlit(self):
         raise NotImplementedError
 
-
+    # def test_readin(filename: str):
+    #     if data_upload(filename) == False:
+    #         print("filename did not upload")
+    #         raise IOError
 if __name__ == "__main__":
     unittest.main()
