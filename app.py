8b86937f4b5a4a8e83fe0b644e277529b5de1f87
Nile Inel <nileinel@Niles-MacBook-Air.local>
2022-11-26 20:11:40 +0000

    File upload ported over. Progress on Issue #24.

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 import streamlit as st
+import os
+import sys
+# import mvexperiment.experiment as experiment
+import numpy as np
+
+
+def open_folder(root_name: str):
+    pass
+
+
+def save_uploadedfile(uploadedfile):
+    with open(os.path.join("data", uploadedfile.name), "wb") as f:
+        f.write(uploadedfile.getbuffer())
+    return st.success("Saved File:{} to data".format(uploadedfile.name))
 
 
 def main() -> None:
-    pass
+    file = st.file_uploader("Choose a zip file")
+    if file is not None:
+        save_uploadedfile(file)
 
 
 if __name__ == "__main__":
