0781e47c0879c58f2e19390e2af0fd0e2fdba7c5
Nile Inel <nileinel@Niles-MacBook-Air.local>
2023-02-12 22:11:10 +0000

    NanoAnalysis page created. app.py renamed to NanoPrepare.py and references are updated to show page titles correctly. Progress toward Issue #40

---
+++ pages/NanoAnalysis.py
@@ -0,0 +1,24 @@
+import calendar
+import tempfile
+import streamlit as st
+from streamlit.runtime.uploaded_file_manager import UploadedFile
+import os
+import sys
+import mvexperiment.experiment as experiment
+import numpy as np
+import zipfile
+import matplotlib.pyplot as plt
+import pandas as pd
+import json
+import shutil
+import altair as alt
+import nanodata
+import nanodata.nano as nano
+
+
+def main() -> None:
+    st.title('NanoAnalysis')
+
+
+if __name__ == "__main__":
+    main()
