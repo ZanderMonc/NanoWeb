a29b7b7571cf1765bb82486e43b3265dd51f11d6
Nile Inel <nileinel@Niles-MacBook-Air.local>
2023-02-14 00:15:08 +0000

    Layered chart of raw curves displaying all curves layered on each other is functional. Progress towards Issue #40

--- pages/NanoAnalysis.py
+++ pages/NanoAnalysis.py
@@ -14,16 +14,30 @@ import shutil
 import altair as alt
 import nanodata
 import nanodata.nano as nano
-from NanoPrepare import save_uploaded_file
+from NanoPrepare import save_uploaded_file, base_chart, layer_charts
 import nanoanalysisdata.engine as engine
 
 
-def handle_click(i):
+def handle_click(i: int) -> None:
     # activate and deactivate curve in haystack on checkbox click
     if engine.haystack[i].active:
         engine.haystack[i].active = False
     else:
         engine.haystack[i].active = True
+
+
+def generate_raw_curves(haystack: list) -> list:
+    all_curves = []
+    for curve in haystack:
+        if curve.active:
+            df = pd.DataFrame(
+                {
+                    "z": curve.data["Z"],
+                    "f": curve.data["F"],
+                }
+            )
+        all_curves.append(df)
+    return all_curves
 
 
 def main() -> None:
@@ -55,11 +69,18 @@ def main() -> None:
             for i, curve in enumerate(engine.haystack):
                 graph_first_col.checkbox(curve.filename, value=True, key=i, on_change=handle_click, args=(i,))
 
-            # Raw curve plots
+            # Raw curve plot
             graph_second_col_raw = graph_second_col.container()
             graph_second_col_raw.write("Raw curves")
             graph_second_col_raw_plot = graph_second_col_raw.line_chart()
+            raw_curves = generate_raw_curves(engine.haystack)
 
+            graph_second_col_raw_plot.altair_chart(
+                layer_charts(raw_curves, base_chart),
+                use_container_width=True
+            )
+
+            # Current curve plot
             graph_second_col_current = graph_second_col.container()
             graph_second_col_current.write("Current curve")
             graph_second_col_current_plot = graph_second_col_current.line_chart()
