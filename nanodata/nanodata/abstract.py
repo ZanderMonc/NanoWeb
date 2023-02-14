c49b856eadad820fcf690e690110d6e01a2233be
OSA\devlu <devlukaszwysocki@gmail.com>
2023-02-14 01:00:37 +0000

    Changed AbstractDataManager to DataManager and added basic functionality. Changed abc import to increase clarity.

--- nanodata/nanodata/abstract.py
+++ nanodata/nanodata/abstract.py
@@ -1,5 +1,6 @@
-from abc import ABC, abstractmethod
-from typing import Iterable, Optional, TypeVar, Any, Generic
+import abc
+import os
+from typing import Iterable, Optional, TypeVar, Any, Generic, Iterator
 
 from nanodata.nanodata.errors import AbstractNotImplementedError
 from . import interfaces
@@ -19,66 +20,99 @@ class DataSetTypeExistsError(Exception):
     pass
 
 
-class AbstractDataManager(ABC, Generic[TDataSet]):
-    """Abstract class for managing data sets.
+class DataManager(
+    interfaces.IDataManager[interfaces.TDataSet, interfaces.TDataSetType], abc.ABC
+):
+    def __init__(self, path: str):
+        self._path: str = path
+        self._data_sets: dict[str, interfaces.TDataSet] = {}
+        self._file_types: list[interfaces.TDataSetType] = []
 
-    Is effectively a dictionary of data sets which points to top level directory.
+    def _add_data_set(self, data_set: interfaces.TDataSet) -> None:
+        if data_set.name in self._data_sets:
+            raise DataSetExistsError(
+                f"Data set with name '{data_set.name}' already exists."
+            )
+        self._data_sets[data_set.name] = data_set
 
-    Args:
-        dir_path (str): Path to the directory containing the data sets.
-    """
+    def _remove_data_set(self, name: str) -> None:
+        if name in self._data_sets:
+            del self._data_sets[name]
+        else:
+            raise DataSetNotFoundError(f"Data set with name '{name}' not found.")
 
-    def __init__(self, dir_path: str):
-        self._path: str = dir_path
-        self._data_sets: dict[str, TDataSet] = {}
-        self._file_types: list[TDataSetType] = []
+    def register_file_type(self, file_type: interfaces.TDataSetType) -> None:
+        if file_type not in self._file_types:
+            self._file_types.append(file_type)
+        else:
+            raise DataSetTypeExistsError(f"Data set type '{file_type}' already exists.")
 
-    def add_data_set(self) -> None:
-        raise AbstractNotImplementedError()
+    def load(self) -> None:
+        for directory in os.walk(self.path):
+            file_names = directory[2]
+            for file_name in file_names:
+                file_path = os.path.join(directory[0], file_name)
+                self.load_file(file_path)
 
-    @abstractmethod
-    def remove_data_set(self, name: str) -> None:
-        raise AbstractNotImplementedError()
+    def load_file(self, file_path: str) -> None:
+        file_name, _ = os.path.splitext(file_path)
+        for file_type in self._file_types:
+            if file_type.is_valid(file_path):
+                data_set = file_type.create_data_set(file_name, file_path)
+                # TODO move load to separate method
+                data_set.load()
+                self._add_data_set(data_set)
+                break
 
-    @abstractmethod
-    def get_data_set(self, name: str) -> Optional[TDataSet]:
-        raise AbstractNotImplementedError()
-
-    @abstractmethod
-    def register_file_type(self, file_type: TDataSetType) -> None:
-        raise AbstractNotImplementedError()
-
-    @abstractmethod
-    def preload(self) -> None:
-        raise AbstractNotImplementedError()
-
-    @abstractmethod
-    def unzip(self) -> None:
-        raise AbstractNotImplementedError()
-
-    @abstractmethod
     def load_data_set(self, name: str) -> None:
-        raise AbstractNotImplementedError()
+        if name in self._data_sets:
+            self._data_sets[name].load()
+        else:
+            raise DataSetNotFoundError(f"Data set with name '{name}' not found.")
 
-    @abstractmethod
+    def apply_filter(self):
+        # TODO IMPLEMENT
+        # ? Passing entire data set object probably not efficient
+        # ? Should get filter params and match those to instance vars of data set
+        pass
+
     def clear(self) -> None:
-        raise AbstractNotImplementedError()
+        self._data_sets.clear()
 
-    @abstractmethod
+    @property
+    def values(self) -> Iterable[interfaces.TDataSet]:
+        return self._data_sets.values()
+
+    @property
+    def items(self) -> Iterable[tuple[str, interfaces.TDataSet]]:
+        return self._data_sets.items()
+
+    @property
     def keys(self) -> Iterable[str]:
-        raise AbstractNotImplementedError()
+        return self._data_sets.keys()
 
-    @abstractmethod
-    def values(self) -> Iterable[TDataSet]:
-        raise AbstractNotImplementedError()
-
-    @abstractmethod
-    def items(self) -> Iterable[tuple[str, TDataSet]]:
-        raise AbstractNotImplementedError()
-
-    @abstractmethod
+    @property
     def path(self) -> str:
-        raise AbstractNotImplementedError()
+        return self._path
+
+    @property
+    def datasets(self) -> Iterable[interfaces.TDataSet]:
+        return self._data_sets.values()
+
+    def __getitem__(self, name: str) -> interfaces.TDataSet:
+        if name in self._data_sets:
+            return self._data_sets[name]
+        else:
+            raise DataSetNotFoundError(f"Data set with name '{name}' not found.")
+
+    def __len__(self) -> int:
+        return len(self._data_sets)
+
+    def __iter__(self) -> Iterator[interfaces.TDataSet]:
+        return iter(self._data_sets.values())
+
+    def __repr__(self) -> str:
+        return f"{self.__name__}(path={self.path!r}, data_sets={len(self)!r}, file_types={self._file_types!r})"
 
 
 class AbstractDataSet(ABC):
