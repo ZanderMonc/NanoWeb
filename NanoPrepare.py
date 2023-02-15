19d8da1830074aa7da38f44ab2126449c95bb725
ZanderMonc <98114770+ZanderMonc@users.noreply.github.com>
2023-02-15 09:38:19 +0000

    Update NanoPrepare.py

--- NanoPrepare.py
+++ NanoPrepare.py
@@ -1,7 +1,8 @@
+from __future__ import annotations
 import tempfile
 import streamlit as st
 from streamlit.runtime.uploaded_file_manager import UploadedFile
-from __future__ import annotations
+
 import os
 import mvexperiment.experiment as experiment
 import numpy as np
