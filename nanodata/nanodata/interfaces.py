e16fc1fe50815f8458952bc61b940f77eab2739d
2381681W <2381681w@student.gla.ac.uk>
2023-03-01 13:24:43 +0000

    Changed interface methods to use "..." instead of "pass"

--- nanodata/nanodata/interfaces.py
+++ nanodata/nanodata/interfaces.py
@@ -18,7 +18,7 @@ class Singleton(type):
 
 
 class AbstractSingleton(Singleton, abc.ABCMeta):
-    pass
+    ...
 
 
 class IDataManager(
@@ -26,191 +26,191 @@ class IDataManager(
 ):
     @abc.abstractmethod
     def _add_data_set(self, data_set: TDataSet) -> None:
-        pass
+        ...
 
     @abc.abstractmethod
     def _remove_data_set(self, name: str) -> None:
-        pass
+        ...
 
     @abc.abstractmethod
     def register_file_type(self, file_type: TDataSetType) -> None:
-        pass
+        ...
 
     @abc.abstractmethod
     def load(self) -> None:
-        pass
+        ...
 
     @abc.abstractmethod
     def load_data_set(self, name: str) -> None:
-        pass
+        ...
 
     @abc.abstractmethod
     def apply_filter(self):
-        pass
+        ...
 
     @abc.abstractmethod
     def clear(self) -> None:
-        pass
+        ...
 
     @property
     @abc.abstractmethod
     def values(self) -> Iterable[TDataSet]:
-        pass
+        ...
 
     @property
     @abc.abstractmethod
     def items(self) -> Iterable[tuple[str, TDataSet]]:
-        pass
+        ...
 
     @property
     @abc.abstractmethod
     def keys(self) -> Iterable[str]:
-        pass
+        ...
 
     @property
     @abc.abstractmethod
     def path(self) -> str:
-        pass
+        ...
 
     @property
     @abc.abstractmethod
     def data_sets(self) -> Iterable[TDataSet]:
-        pass
+        ...
 
     @abc.abstractmethod
     def __getitem__(self, name: str) -> TDataSet:
-        pass
+        ...
 
     @abc.abstractmethod
     def __len__(self) -> int:
-        pass
+        ...
 
     @abc.abstractmethod
     def __iter__(self) -> Iterable[TDataSet]:
-        pass
+        ...
 
     @abc.abstractmethod
     def __repr__(self) -> str:
-        pass
+        ...
 
 
 class IDataSet(abc.ABC):
     @abc.abstractmethod
     def load(self) -> None:
-        pass
+        ...
 
     @property
     @abc.abstractmethod
     def name(self) -> str:
-        pass
+        ...
 
     @property
     @abc.abstractmethod
     def path(self) -> str:
-        pass
+        ...
 
     @property
     @abc.abstractmethod
     def active(self) -> bool:
-        pass
+        ...
 
     @abc.abstractmethod
     def activate(self) -> None:
-        pass
+        ...
 
     @abc.abstractmethod
     def deactivate(self) -> None:
-        pass
+        ...
 
     @abc.abstractmethod
     def __repr__(self) -> str:
-        pass
+        ...
 
 
 class IDataSetType(abc.ABC):
     @abc.abstractmethod
     def is_valid(self, path: str) -> bool:
-        pass
+        ...
 
     @abc.abstractmethod
     def create_data_set(self, name: str, path: str) -> TDataSet:
-        pass
+        ...
 
     @abc.abstractmethod
     def has_valid_extension(self, path: str) -> bool:
-        pass
+        ...
 
     @abc.abstractmethod
     def has_valid_header(self, path: str) -> bool:
-        pass
+        ...
 
     @property
     @abc.abstractmethod
     def extensions(self) -> Iterable[str]:
-        pass
+        ...
 
     @property
     @abc.abstractmethod
     def name(self) -> str:
-        pass
+        ...
 
 
 class ISegment(abc.ABC):
     @property
     @abc.abstractmethod
     def time(self) -> np.ndarray:
-        pass
+        ...
 
     @time.setter
     @abc.abstractmethod
     def time(self, value: np.ndarray):
-        pass
+        ...
 
     @property
     @abc.abstractmethod
     def force(self) -> np.ndarray:
-        pass
+        ...
 
     @force.setter
     @abc.abstractmethod
     def force(self, value: np.ndarray):
-        pass
+        ...
 
     @property
     @abc.abstractmethod
     def deflection(self) -> np.ndarray:
-        pass
+        ...
 
     @deflection.setter
     @abc.abstractmethod
     def deflection(self, value: np.ndarray):
-        pass
+        ...
 
     @property
     @abc.abstractmethod
     def z(self) -> np.ndarray:
-        pass
+        ...
 
     @z.setter
     @abc.abstractmethod
     def z(self, value: np.ndarray):
-        pass
+        ...
 
     @property
     @abc.abstractmethod
     def indentation(self) -> np.ndarray:
-        pass
+        ...
 
     @indentation.setter
     @abc.abstractmethod
     def indentation(self, value: np.ndarray):
-        pass
+        ...
 
     @property
     @abc.abstractmethod
     def data(self) -> dict[str, Any]:
-        pass
+        ...
 
     @abc.abstractmethod
     def __repr__(self) -> str:
-        pass
+        ...
