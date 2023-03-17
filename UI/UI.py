ddbb2051905638950a7b94779d8fcf671ccc2def
2381681W <2381681w@student.gla.ac.uk>
2023-03-17 18:10:31 +0000

    Added UIElement template for future UI elements

--- UI/UI.py
+++ UI/UI.py
@@ -2,6 +2,7 @@ import streamlit as st
 from misc import *
 import tempfile
 import nanodata as nd
+import abc
 
 
 class UI(st.delta_generator.DeltaGenerator, metaclass=UISingleton):
@@ -44,3 +45,48 @@ class UI(st.delta_generator.DeltaGenerator, metaclass=UISingleton):
                 "\nGithub: https://github.com/CellMechLab/",
             },
         )
+
+
+class UIElement(abc.ABC):
+    def __init__(self, window: UI):
+        self._window = window
+
+    @abc.abstractmethod
+    def draw(self):
+        pass
+
+    @abc.abstractmethod
+    def write(self, *args, **kwargs) -> None:
+        pass
+
+    @abc.abstractmethod
+    def header(self, *args, **kwargs) -> None:
+        pass
+
+    @abc.abstractmethod
+    def button(self, *args, **kwargs) -> bool:
+        pass
+
+    @abc.abstractmethod
+    def number_input(self, *args, **kwargs) -> int | float:
+        pass
+
+    @abc.abstractmethod
+    def slider(self, *args, **kwargs) -> Any:
+        pass
+
+    @abc.abstractmethod
+    def selectbox(self, *args, **kwargs) -> Any:
+        pass
+
+    @abc.abstractmethod
+    def columns(self, *args, **kwargs) -> Any:
+        pass
+
+    @abc.abstractmethod
+    def expander(self, title: str) -> Any:
+        pass
+
+    @property
+    def window(self):
+        return self._window
