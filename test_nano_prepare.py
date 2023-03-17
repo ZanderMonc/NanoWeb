291680e23450568b0ee6ddae48b752347d71212f
“Fareedh <2617283y@student.gla.ac.uk>
2023-03-17 22:37:33 +0000

    Some missing tests in last merge request

--- test_nano_prepare.py
+++ test_nano_prepare.py
@@ -27,21 +27,32 @@ def test_extract_zip_with_not_zip_file():
 def test_file_handler():
     f = open("tests/smallest.zip", "rb")
     file_name = "tests/smallest.zip"
-    assert isinstance(NanoPrepare.file_handler(file_name, f), nanodata.nanodata.ChiaroDataManager) is True
-
-def test_threshold_filter():
-    #TODO : add a test for the threshold filter when it is finished
-    pass
+    quale = 'quale'
+    assert isinstance(NanoPrepare.file_handler(file_name, quale, f), nanodata.nanodata.ChiaroDataManager) is True
 
 def test_save_to_json():
     #TODO : add a test for the save to json when it is finished
     pass
 
+def test_get_filter_with_existing_filter():
+    assert isinstance(NanoPrepare.get_filter('Force Filter'), nanodata.nanodata.filter.ForceFilter) is True
+
+def test_get_filter_with_none_existing_filter():
+    assert NanoPrepare.get_filter('None existing filter') is None
+
+def test_execute_filter():
+    f = open("tests/smallest.zip", "rb")
+    quale = 'quale'
+    file_name = "tests/smallest.zip"
+    data = NanoPrepare.file_handler(file_name, quale, f)
+    filter_object = NanoPrepare.get_filter('Force Filter')
+    threshold = 0.0
+    params =  {"force": float(threshold),"comparison": ">"}
+
+    assert len(NanoPrepare.execute_filter(data, filter_object, params)) == 3
 
 def test_save_uploaded_file():
-    #TODO : add a test for the save uploaded file when it is finished
     pass
-
 
 def test_generate_json_template():
     curve = {
