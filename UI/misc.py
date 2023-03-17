d07f9b9273a04357b2b42b02e5712ac3b39b53a8
2381681W <2381681w@student.gla.ac.uk>
2023-03-17 18:04:53 +0000

    Added singleton to go around Streamlit's constant code re-running issue.

--- UI/misc.py
+++ UI/misc.py
@@ -1,6 +1,15 @@
 from enum import StrEnum
 
 
+class UISingleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(UISingleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
 class DataSetVars(StrEnum):
     TIME = "time"
     FORCE = "force"
